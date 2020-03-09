7462e03f296da3bd5346ca3768f0c4cd7f7b8942
Daniele Rolando <danielerolando90@gmail.com>
2020-03-09 09:12:24 -0700

    1

--- backend.py
+++ backend.py
@@ -3,21 +3,23 @@
 import datetime
 
 from py_zipkin import Encoding
+from py_zipkin.transport import SimpleHTTPTransport
 from pyramid.config import Configurator
 from pyramid.response import Response
 from pyramid.view import view_config
-from transport import http_transport
 from wsgiref.simple_server import make_server
+
 
 @view_config(route_name='print_date')
 def print_date(request):
     return Response(str(datetime.datetime.now()))
 
+
 def main():
     settings = {}
     settings['service_name'] = 'backend'
     settings['zipkin.encoding'] = Encoding.V2_JSON
-    settings['zipkin.transport_handler'] = http_transport
+    settings['zipkin.transport_handler'] = SimpleHTTPTransport('localhost', 9411)
 
     config = Configurator(settings=settings)
     config.include('pyramid_zipkin')
@@ -28,6 +30,7 @@ def main():
     server = make_server('0.0.0.0', 9000, app)
     print('Backend listening on http://localhost:9000/api')
     server.serve_forever()
+
 
 if __name__ == '__main__':
     main()
--- frontend.py
+++ frontend.py
@@ -6,26 +6,30 @@ from py_zipkin import Encoding
 from pyramid.config import Configurator
 from pyramid.response import Response
 from pyramid.view import view_config
-from py_zipkin.zipkin import create_http_headers_for_new_span
-from transport import http_transport
+from py_zipkin.request_helpers import create_http_headers
+from py_zipkin.transport import SimpleHTTPTransport
+from py_zipkin.zipkin import zipkin_client_span
 from wsgiref.simple_server import make_server
+
 
 @view_config(route_name='call_backend')
 def call_backend(request):
-    headers = {}
-    headers.update(create_http_headers_for_new_span())
-    backend_response = requests.get(
-        url='http://localhost:9000/api',
-        headers=headers,
-    )
-    return Response(backend_response.text)
+    with zipkin_client_span('frontend', 'call_backend'):
+        headers = {}
+        headers.update(create_http_headers())
+        backend_response = requests.get(
+            url='http://localhost:9000/api',
+            headers=headers,
+        )
+        return Response(backend_response.text)
+
 
 def main():
     settings = {}
     settings['service_name'] = 'frontend'
     settings['zipkin.encoding'] = Encoding.V2_JSON
     settings['zipkin.tracing_percent'] = 100.0
-    settings['zipkin.transport_handler'] = http_transport
+    settings['zipkin.transport_handler'] = SimpleHTTPTransport('localhost', 9411)
 
     config = Configurator(settings=settings)
     config.include('pyramid_zipkin')
@@ -37,6 +41,7 @@ def main():
     server = make_server('0.0.0.0', 8081, app)
     print('Frontend listening on http://localhost:8081')
     server.serve_forever()
+
 
 if __name__ == '__main__':
     main()
